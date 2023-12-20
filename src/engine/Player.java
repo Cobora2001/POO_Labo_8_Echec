02880f83500b5d00b24025b0f42a6b9f53214172
SNSantos <dedale.sigma@gmail.com>
2023-12-20 15:07:09 +0100

    Player required functionnalities

--- src/engine/Player.java
+++ src/engine/Player.java
@@ -1,14 +1,33 @@
 package engine;
 
 import chess.PlayerColor;
+import engine.Pieces.Piece;
+
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Player {
+    private final Piece king;
+    private final LinkedList<Piece> pieces = new LinkedList<>();
     private final PlayerColor color;
-
-    public Player(PlayerColor color) {
-        this.color = color;
+    public Piece getKing(){
+        return king;
     }
 
+    public Player(Piece king, PlayerColor color) {
+        this.king = king;
+        this.color = color;
+    }
+    Iterator getIterator(){
+        return pieces.iterator();
+    }
+
+    void removePiece(Piece piece){
+        pieces.remove(piece);
+    }
+    void addPiece(Piece piece){
+        pieces.add(piece);
+    }
     public PlayerColor getColor() {
         return color;
     }
