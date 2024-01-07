b6e9740e3fb68600ab6cbda384b33e3dd426e878
SNSantos <dedale.sigma@gmail.com>
2024-01-07 12:20:01 +0100

    Horizontal AKA rook move

--- src/engine/Vertical.java
+++ src/engine/Vertical.java
@@ -2,8 +2,6 @@ package engine;
 
 public class Vertical extends Ruleset{
     public boolean movePossible(Piece piece, int toX, int toY) { // can't be static if overridden
-        return super.movePossible( piece, toX, toY)
-        && piece.getX() == toX;
-
+        return piece.getX() == toX;
     }
 }
