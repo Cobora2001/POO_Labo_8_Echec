d87ef3e2db54ce483a869dfb2d68486945b14b87
Cobora2001 <thomasvu@bluewin.ch>
2024-01-13 14:45:12 +0100

    comments for Bishop and CaresAboutObstacles

--- src/engine/CaresAboutObstacles.java
+++ src/engine/CaresAboutObstacles.java
@@ -1,6 +1,19 @@
+/**
+ * CaresAboutObstacles.java: represents a ruleset that cares about
+ * obstacles in the matrix of the engine
+ */
+
 package engine;
 
 public abstract class CaresAboutObstacles extends Ruleset{
+    /**
+     * Check if the piece can move to the given position
+     * @param piece the piece
+     * @param toX the x position
+     * @param toY the y position
+     * @param engine the engine
+     * @return null if the piece can move to the given position, and a message explaining why it can't otherwise
+     */
     public String availableMove(Piece piece, int toX, int toY, Engine engine) { // can't be static if overridden
         String response = super.availableMove(piece, toX, toY, engine);
 
@@ -14,7 +27,15 @@ public abstract class CaresAboutObstacles extends Ruleset{
         return null;
     }
 
-
+    /**
+     * Check if the way the piece wants to move is obstructed
+     * @param piece the piece
+     * @param toX the x position
+     * @param toY the y position
+     * @param matrix the matrix
+     * @return true if the piece can move to the given position without anything in its way,
+     * false otherwise
+     */
     public boolean isObstructed(Piece piece, int toX, int toY, Piece[][] matrix) {
         int fromX = piece.getX();
         int fromY = piece.getY();
@@ -22,13 +43,13 @@ public abstract class CaresAboutObstacles extends Ruleset{
         int diffX = toX - fromX;
         int diffY = toY - fromY;
 
+        // We check if the piece is moving in a straight line
         if(Math.abs(diffX) == Math.abs(diffY)) {
             if(diffX == 0) {
                 return false;
             }
         } else {
             if(diffX != 0 && diffY != 0) {
-                // Pas en ligne droite
                 return true;
             }
         }
@@ -39,6 +60,7 @@ public abstract class CaresAboutObstacles extends Ruleset{
         fromX += changeX;
         fromY += changeY;
 
+        // We check if there is a piece in the way
         while(!(fromX == toX && fromY == toY)) {
             if(matrix[fromX][fromY] != null) {
                 return true;
@@ -50,6 +72,4 @@ public abstract class CaresAboutObstacles extends Ruleset{
 
         return false;
     }
-
-
 }
