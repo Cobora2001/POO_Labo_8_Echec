da1bf2d43fe6b9b6afe75284a3f30c74054c5c52
Cobora2001 <thomasvu@bluewin.ch>
2024-01-13 14:30:51 +0100

    add comments for piece

--- src/engine/Piece.java
+++ src/engine/Piece.java
@@ -1,16 +1,25 @@
+/**
+ * Piece: abstract class for all pieces
+ */
+
 package engine;
 
 import chess.PieceType;
 import chess.PlayerColor;
 
 abstract public class Piece {
+    // coordinates of the piece
     private int x;
     private int y;
 
     private final PlayerColor color;
 
-
-
+    /**
+     * Constructor
+     * @param x: x coordinate
+     * @param y: y coordinate
+     * @param color: color of the piece
+     */
     public Piece( int x, int y, PlayerColor color) {
         this.x = x;
         this.y = y;
@@ -18,30 +27,60 @@ abstract public class Piece {
 
     }
 
+    /**
+     * Get the type of the piece
+     * @return the type of the piece
+     */
     abstract public PieceType getPieceType();
 
+    /**
+     * Get the x coordinate of the piece
+     * @return the x coordinate of the piece
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Get the y coordinate of the piece
+     * @return the y coordinate of the piece
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Get the color of the piece
+     * @return the color of the piece
+     */
     public PlayerColor getColor() {
         return color;
     }
 
-
+    /**
+     * Set the coordinates of the piece
+     * @param x: x coordinate
+     * @param y: y coordinate
+     */
     public void setCoordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Set the coordinates of the piece
+     * @param x: x coordinate
+     * @param y: y coordinate
+     */
     abstract public String canMove(int x, int y, Engine engine) ;
        // return ruleset.availableMove(this, x, y, engine);
 
-
+    /**
+     * Update the matrix of the engine according to the move of the piece
+     * @param toX destination x coordinate
+     * @param toY destination y coordinate
+     * @param engine the engine with the matrix to update
+     */
     abstract public void updateMatrix(int toX, int toY, Engine engine) ;
        // ruleset.updateMatrix(this, toX, toY, engine);
 }
